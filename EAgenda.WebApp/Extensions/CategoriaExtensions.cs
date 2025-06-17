4b695bf39414955d115b94fbabc4e3551e3442b1
GustavoW <guvieirawalter@gmail.com>
2025-06-17 15:47:04 -0300

    Implementado ViewModel e Extensions para "Categoria"

---
+++ EAgenda.WebApp/Extensions/CategoriaExtensions.cs
@@ -0,0 +1,18 @@
+﻿using EAgenda.Dominio.ModuloCategoria;
+using EAgenda.WebApp.Models;
+using static EAgenda.WebApp.Models.VisualizarCategoriaViewModel;
+
+namespace EAgenda.WebApp.Extensions;
+
+public static class CategoriaExtensions
+{
+    public static DetalhesCategoriaViewModel ParaDetalhesVM(this Categoria categoria)
+    {
+        return new DetalhesCategoriaViewModel
+        {
+            Id = categoria.Id,
+            Titulo = categoria.Titulo,
+            Despesas = categoria.Despesas,
+        };
+    }
+}
---
+++ EAgenda.WebApp/Models/CategoriaViewModel.cs
@@ -0,0 +1,89 @@
+﻿using EAgenda.Dominio.ModuloCategoria;
+using EAgenda.Dominio.ModuloDespesa;
+using EAgenda.WebApp.Extensions;
+using System.ComponentModel.DataAnnotations;
+namespace EAgenda.WebApp.Models;
+
+public abstract class FormularioCategoriaViewModel
+{
+    [Required(ErrorMessage = "O campo \"Título\" é obrigatório.")]
+    [MinLength(2, ErrorMessage = "O campo \"Título\" deve ter no mínimo 2 caracteres.")]
+    [MaxLength(100, ErrorMessage = "O campo \"Título\" deve ter no máximo 100 caracteres.")]
+    public string Titulo { get; set; }
+    public List<Despesa> Despesas { get; set; } = new List<Despesa>(); 
+}
+
+public class CadastrarCategoriaViewModel : FormularioCategoriaViewModel
+{
+    public CadastrarCategoriaViewModel() { }
+
+    public CadastrarCategoriaViewModel(string titulo, List<Despesa> despesas) : this()
+    {
+        Titulo = titulo;
+        Despesas = despesas;
+    }
+}
+
+public class EditarCategoriaViewModel : FormularioCategoriaViewModel
+{
+    public Guid Id { get; set; }
+
+    public EditarCategoriaViewModel() { }
+
+    public EditarCategoriaViewModel(Guid id, string titulo, List<Despesa> despesas)
+    {
+        Id = id;
+        Titulo = titulo;
+        Despesas = despesas;
+    }
+}
+
+
+public class ExcluirCategoriaViewModel
+{
+    public Guid Id { get; set; }
+    public string Titulo { get; set; }
+
+    public ExcluirCategoriaViewModel(Guid id, string titulo)
+    {
+        Id = id;
+        Titulo = titulo;
+    }
+}
+
+public class VisualizarCategoriaViewModel
+{
+    public List<DetalhesCategoriaViewModel> Registros { get; } = new List<DetalhesCategoriaViewModel>();
+
+    public VisualizarCategoriaViewModel(List<Categoria> categorias)
+    {
+        foreach (Categoria c in categorias)
+            Registros.Add(c.ParaDetalhesVM());
+    }
+
+    public class DetalhesCategoriaViewModel
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public List<Despesa> Despesas { get; set; }
+        public DetalhesCategoriaViewModel() { }
+        public DetalhesCategoriaViewModel(Guid id, string titulo, List<Despesa> despesas)
+        {
+            Id = id;
+            Titulo = titulo;
+            Despesas = despesas;
+        }
+    }
+
+    public class SelecionarCategoriaViewModel
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+
+        public SelecionarCategoriaViewModel(Guid id, string titulo)
+        {
+            Id = id;
+            Titulo = titulo;
+        }
+    }
+}
