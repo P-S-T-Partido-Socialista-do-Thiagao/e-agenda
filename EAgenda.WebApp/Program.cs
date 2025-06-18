d8a9c2a45a75a1fba36e4a462e9eb322b4eefac1
Victor R. Silva <victorr.silva005@gmail.com>
2025-06-18 15:11:18 -0300

    corrigido pastas do bootstrap

--- EAgenda.WebApp/Program.cs
+++ EAgenda.WebApp/Program.cs
@@ -5,9 +5,15 @@ namespace EAgenda.WebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseAntiforgery();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
