54d9433ad7ad981d7cdacf4a26efb829f574509d
GustavoW <guvieirawalter@gmail.com>
2025-06-18 14:39:46 -0300

    Implementado Controller para Compromisso

---
+++ EAgenda.WebApp/Controllers/DespesaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EAgenda.WebApp.Controllers
+{
+    public class DespesaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
