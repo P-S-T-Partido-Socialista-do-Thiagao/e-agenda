6671f34ddf5ac0843cd308d913630eff5958a59d
Victor R. Silva <victorr.silva005@gmail.com>
2025-06-22 12:27:11 -0300

    Implementado visualização de despesas por categoria

--- EAgenda.Dominio/ModuloCategoria/IRepositorioCategoria.cs
+++ EAgenda.Dominio/ModuloCategoria/IRepositorioCategoria.cs
@@ -1,8 +1,10 @@
 ﻿using EAgenda.Dominio.Compartilhado;
+using EAgenda.Dominio.ModuloDespesa;
 
 namespace EAgenda.Dominio.ModuloCategoria
 {
     public interface IRepositorioCategoria : IRepositorio<Categoria>
     {
+        List<Despesa> SelecionarDespesasPorCategoria(Guid idCategoria);
     }
 }
